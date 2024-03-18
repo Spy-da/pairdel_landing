cb1088db0989879320b2455b651b2417c8a05dda
Onifade Joshua <onifadjosh@gmail.com>
2024-03-18 23:04:56 +0100

    Major navbar update

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -38,13 +38,50 @@ export default function Nav() {
         setIsOpen(!isOpen);
     };
     return (
-        <div id='navbar' className={`navMenu mt-[32px] font-[600] text-[20px] leading-[28px] fixed z-[50] ml-[24px] xl:ml-[128px] flex gap-[64px] w-fit py-[16px] pl-[32px] pr-[16px] rounded-[4000px]  items-center text-[#FFFFFF]  bg-gradient-to-b from-[#FFFFFF1F] to-[#FFFFFF1F] backdrop-blur-[20px]`}>
-            <Link href={'/'} className='flex items-center gap-[11.76px]'>
-                <Image width={35.11} height={36} src={'/pairdelLogo.svg'} alt='logo'/>
-                <p>Pairdel</p> 
-            </Link>
+        <div id='navbar' className={`font-[600] text-[20px] leading-[28px]  w-full fixed text-[#FFFFFF] z-[80]`}>
+            <div className={`mx-[24px] barbar pl-[24px] md:pl-[32px] pr-[10px] md:pr-[16px] flex justify-between items-center mt-[32px]  xl:ml-[128px] bg-gradient-to-b from-[#FFFFFF1F] to-[#FFFFFF1F] backdrop-blur-[20px] md:w-fit py-[10px] md:py-[16px] gap-[64px] rounded-[4000px] ${isOpen ? "active" : ""}`}>
+                <Link href={'/'} className='flex items-center gap-[11.76px]'>
+                    <Image width={35.11} height={36} src={'/pairdelLogo.svg'} alt='logo'/>
+                    <p>Pairdel</p> 
+                </Link>
 
-            <div className='flex gap-[12px] items-center'>
+                <div
+                    className={`/flex gap-[12px] hidden  md:flex items-center justify-between`}
+                >
+                    <span className='/hidden md:block flex flex-col justify-center items-start'>
+                        <Link href={'/'} className='px-[20px] py-[10px]'>
+                            Blog
+                        </Link>
+
+                        <Link href={'/'} className='px-[20px] py-[10px]'>
+                            Help Center
+                        </Link>
+
+                        <a href="http://" className='px-[26px] py-[14px] bg-[#1E2F28]  rounded-[4000px] font-[700] text-[20px] leading-[28px]'>
+                            Get the app
+                        </a>
+                    </span>
+
+                    
+                </div>
+
+
+                <button
+                        type="button"
+                        className={`hamburger  bg-[#1E2F28]  /flex /flex-col /gap-[10px]  md:hidden ${isOpen ? "active" : ""}`}
+                        onClick={toggleMenu}
+                    >
+                        
+                        <div className="hamburger-bar top"></div>
+                        <div className="hamburger-bar middle"></div>
+                        <div className="hamburger-bar bottom"></div>
+                        
+                    </button>
+            </div>
+
+            <span className={`/hidden md:hidden flex flex-col justify-center px-[24px] gap-[24px] items-start navMenu ${
+                        isOpen ? " active " : ""
+                    }`}  >
                 <Link href={'/'} className='px-[20px] py-[10px]'>
                     Blog
                 </Link>
@@ -56,17 +93,8 @@ export default function Nav() {
                 <a href="http://" className='px-[26px] py-[14px] bg-[#1E2F28]  rounded-[4000px] font-[700] text-[20px] leading-[28px]'>
                     Get the app
                 </a>
-            </div>
-
-            {/* <button
-                type="button"
-                className={`hamburger lg:hidden ${isOpen ? "active" : ""}`}
-                onClick={toggleMenu}
-            >
-                <div className="hamburger-bar top"></div>
-                <div className="hamburger-bar middle"></div>
-                <div className="hamburger-bar bottom"></div>
-            </button> */}
+            </span>
+    
         </div>
     )
 }
